84b863182acabddbefabb78b536c606bbeb6f03d
Flynn Madsen <flynnisobelhugym.madsen@skola.botkyrka.se>
2021-01-18 14:23:59 +0100

    cleanup

--- main.js
+++ main.js
@@ -1,26 +1,20 @@
 
 function caesarEncrypt(msg) {
-    let res = ""
 
     const charCodes = []
-
     for (let i = 0; i < msg.length; i++) {
         charCodes.push(msg[i].charCodeAt(0))
-
     }
 
     for (let i = 0; i < charCodes.length; i++) {
         charCodes[i] = charCodes[i] + 1
-
     }
 
     let result = ""
-
     for (let i = 0; i < charCodes.length; i++) {
         result = result + String.fromCharCode(charCodes[i])
-
-
     }
+    
     return result
 }
 const secretMessage = "ATTACKATDAWN"
