af7495e8aca0392c3c3efac654ffc645ced99228
Flynn Madsen <flynnisobelhugym.madsen@skola.botkyrka.se>
2021-01-18 16:02:48 +0100

    refactor functions in code

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
+import {caesarEncrypt} from "./diycrypto.js"
 
-function caesarEncrypt(msg) {
-
-    const charCodes = []
-    for (let i = 0; i < msg.length; i++) {
-        charCodes.push(msg[i].charCodeAt(0))
-    }
-
-    for (let i = 0; i < charCodes.length; i++) {
-        charCodes[i] = charCodes[i] + 1
-    }
-
-    let result = ""
-    for (let i = 0; i < charCodes.length; i++) {
-        result = result + String.fromCharCode(charCodes[i])
-    }
-    
-    return result
-}
-const secretMessage = "ATTACKATDAWN"
-const encryptedMessage = caesarEncrypt(secretMessage)
-console.log(secretMessage, encryptedMessage)
+console.log(caesarEncrypt("HELLO", 6))
